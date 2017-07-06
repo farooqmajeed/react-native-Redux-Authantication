91996e1c9e46e077d2aace3c050e53bc112965c5
farooqmajeed <farooq306@hotmail.com>
2017-07-06 17:51:00 +0500

    intializing reducer

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -9,12 +9,13 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
+    console.log(action);
     switch (action.type) {
         case EMAIL_CHANGED:
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
-        default:
+         default:
             return state;
     }
 };  
