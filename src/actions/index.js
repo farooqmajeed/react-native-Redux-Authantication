44c4d6e165db540cd03f0aebb070ff0b4ab96dce
farooqmajeed <farooq306@hotmail.com>
2017-07-06 17:50:41 +0500

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,29 @@
-import { 
+import firebase from 'firebase';
+import thunk from 'redux-thunk'
+import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED
-    } from './types';
+} from './types';
 
 export const emailChanged = (text) => {
-    return{
-        type: 'EMAIL_CHANGED ', 
+    return {
+        type: 'EMAIL_CHANGED ',
         payload: text
     };
 };
 
 export const passwordChanged = (text) => {
-    return{
-        type: 'PASSWORD_CHANGED ', 
+    return {
+        type: 'PASSWORD_CHANGED ',
         payload: text
     };
 };
+
+export const loginUser = ({ email, password }) => {
+    return (dispatch) => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => {
+                dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+            });
+    };
+};  
