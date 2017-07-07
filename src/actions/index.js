09ec8b066573bb88c7e806eb3c9c52490ae6e30d
farooqmajeed <farooq306@hotmail.com>
2017-07-07 17:41:05 +0500

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,9 @@ import firebase from 'firebase';
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
-    LOGIN_USER_SUCCESS
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
+    LOGIN_USER
 } from './types';
 
 export const emailChanged = (text) => {
@@ -14,23 +16,37 @@ export const emailChanged = (text) => {
 };
 
 export const passwordChanged = (text) => {
-    console.log("passwordChanged", text); 
-    
+    console.log("passwordChanged", text);
+
     return {
-        type: PASSWORD_CHANGED ,
+        type: PASSWORD_CHANGED,
         payload: text
     };
 };
 
 export const loginUser = ({ email, password }) => {
-    // console.log("Error", email);
     return (dispatch) => {
+        dispatch({ type: LOGIN_USER  })
+
         firebase.auth().signInWithEmailAndPassword(email, password)
-          .then(user => {
-                dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
-                console.log("email", user)
-            })
-            .catch(err=>console.log(err))
-            
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch((error) => {
+                console.log(error);
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSuccess(dispatch, user))
+                    .catch(() => loginUserFail(dispatch));
+            });
+
     };
-};  
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    })
+};
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
+};
