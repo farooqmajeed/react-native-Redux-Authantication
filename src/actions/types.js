5d2eae564b401ed4162094ad4157b90b2f0123f4
farooqmajeed <farooq306@hotmail.com>
2017-07-07 16:26:29 +0500

    actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,4 @@
+
 export const EMAIL_CHANGED = 'email_changed';
-export const PASSWORD_CHANGED = 'password_Changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS-success';
