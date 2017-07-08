bab0b53f34942ef8636cc3ca8be0926fb7d65d45
farooqmajeed <farooq306@hotmail.com>
2017-07-08 19:51:59 +0500

    EmpolyeeeLIST component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, CardSection } from './common'
 
 class EmployeeList extends Component {
     render() {
         return (
-            <View>
-                <Text> Employee List </Text>
-                <Text> Employee List </Text>
-                <Text> Employee List </Text>
-                <Text> Employee List </Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Text> Employee List </Text>
+                </CardSection>
+                <CardSection>
+                    <Text> Employee List </Text>
+                </CardSection>
+                <CardSection>
+                    <Text> Employee List </Text>
+                </CardSection>
+                <CardSection>
+                    <Text> Employee List </Text>
+                </CardSection>
+
+            </Card>
         );
     }
 }
