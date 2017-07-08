935b776b9838acc930d1713d8de77cc41a28476f
farooqmajeed <farooq306@hotmail.com>
2017-07-08 19:07:04 +0500

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
@@ -44,7 +45,8 @@ const loginUserSuccess = (dispatch, user) => {
     dispatch({
         type: LOGIN_USER_SUCCESS,
         payload: user
-    })
+    });
+    Actions.main();
 };
 
 const loginUserFail = (dispatch) => {
