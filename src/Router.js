83a089d9a693ba1d05a026e5886116ba09e0067b
farooqmajeed <farooq306@hotmail.com>
2017-07-08 18:17:45 +0500

    rendering app

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Scene, Router } from 'reac 
-t-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
+    return( 
     <Router sceneStyle={{ paddingTop: 65 }}>
         <Scene key="auth">
             <Scene key="login" component={LoginForm} title="Please login" />
         </Scene>
     </Router>
+    );
 };
 
-export default RouterComponent
+export default RouterComponent;
