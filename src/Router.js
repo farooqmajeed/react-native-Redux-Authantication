5f03a054c08a432a881d967bfa75f2800eaf0da0
farooqmajeed <farooq306@hotmail.com>
2017-07-08 19:07:20 +0500

    Update Router

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
-    return( 
-    <Router sceneStyle={{ paddingTop: 65 }}>
-        <Scene key="auth">
-            <Scene key="login" component={LoginForm} title="Please login" />
-        </Scene>
-    </Router>
+    return (
+        <Router sceneStyle={{ paddingTop: 65 }}>
+            <Scene key="auth">
+                <Scene key="login" component={LoginForm} title="Please login" />
+            </Scene>
+            <Scene key="main">
+                <Scene key="employeeList" component={EmployeeList} title="Employees" />
+            </Scene>
+        </Router>
     );
 };
 
