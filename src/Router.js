ef8a232379ec508d3eaf1b6f06e1bf6eb82a66d3
farooqmajeed <farooq306@hotmail.com>
2017-07-08 19:52:36 +0500

    Update Router

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
 
 const RouterComponent = () => {
     return (
@@ -10,7 +12,15 @@ const RouterComponent = () => {
                 <Scene key="login" component={LoginForm} title="Please login" />
             </Scene>
             <Scene key="main">
-                <Scene key="employeeList" component={EmployeeList} title="Employees" />
+                <Scene
+                    onRight={() => Actions.employeeCreate()}
+                    rightTitle="Add"
+                    key="employeeList"
+                    component={EmployeeList}
+                    title="Employees"
+                    initial
+                    />
+                <Scene key="employeeCreate" component={EmployeeCreate} title="Creae Employees" />
             </Scene>
         </Router>
     );
